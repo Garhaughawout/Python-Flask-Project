2adebdcc58bc3667e52ebb469d8c28cf6645849a
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-06-17 01:00:33 -0400

    added models, need jay to have a look

--- server/models.py
+++ server/models.py
@@ -4,3 +4,77 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    
+    # Relationships
+    properties = db.relationship('Property', backref='owner', lazy=True)
+    visits = db.relationship('Visit', backref='visitor', lazy=True)
+    reviews = db.relationship('Review', backref='author', lazy=True)
+    favorite_properties = db.relationship('FavoriteProperty', back_populates='user')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Property(db.Model):
+    __tablename__ = 'properties'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    # Relationships
+    visits = db.relationship('Visit', backref='property', lazy=True)
+    reviews = db.relationship('Review', backref='property', lazy=True)
+    favorited_by = db.relationship('FavoriteProperty', back_populates='property')
+
+    def __repr__(self):
+        return f'<Property {self.title}>'
+
+class Visit(db.Model):
+    __tablename__ = 'visits'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+    scheduled_date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(50), nullable=False, default='pending')
+    
+    def __repr__(self):
+        return f'<Visit {self.id} by User {self.user_id}>'
+
+class FavoriteProperty(db.Model):
+    __tablename__ = 'favorite_properties'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+    added_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    # Relationships
+    user = db.relationship('User', back_populates='favorite_properties')
+    property = db.relationship('Property', back_populates='favorited_by')
+
+    def __repr__(self):
+        return f'<FavoriteProperty {self.id} by User {self.user_id}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Review {self.id} by User {self.user_id}>'
